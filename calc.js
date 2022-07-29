34388b0ecdbfe3e941dbe363e12c337bc19a7ef1
siryus95 <salaudeenyusuf95@gmail.com>
2022-07-29 10:50:13 +0100

    modified

--- calc.js
+++ calc.js
@@ -1,5 +1,12 @@
-let a, b, c;
+let a, b, c, d, e, f;
 a=window.prompt("enter the first digit");
 b=window.prompt("enter the second digit");
 c=a*b;
+d=a+b;
+e=a-b;
+f=a/b;
+
 alert("Your operation is: "+a+"*"+b+ "="+ c);
+alert("Your operation is: "+a+"+"+b+ "="+ d);
+alert("Your operation is: "+a+"-"+b+ "="+ e);
+alert("Your operation is: "+a+"/"+b+ "="+ f);
