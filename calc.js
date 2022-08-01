7967f35ed3b2a40377339125c084d9b7bd5acf19
siryus95 <salaudeenyusuf95@gmail.com>
2022-08-01 14:29:03 +0100

    mod

--- calc.js
+++ calc.js
@@ -1,4 +1,4 @@
-let a, b, c, d, e, f;
+let a, b, c, d, e, f, g;
 a=window.prompt("enter the first digit");
 b=window.prompt("enter the second digit");
 a=parseFloat(a);
@@ -7,8 +7,11 @@ c=a*b;
 d=a+b;
 e=a-b;
 f=a/b;
+g=a%b;
+
 
 alert("Your operation is: "+a+"*"+b+ "="+ c);
 alert("Your operation is: "+a+"+"+b+ "="+ d);
 alert("Your operation is: "+a+"-"+b+ "="+ e);
 alert("Your operation is: "+a+"/"+b+ "="+ f);
+alert("Your operation is: "+a+"%"+b+ "="+ g);
